1baddfd6a5393c3bc02a8a51cc6bcf4be208ff56
Arief Hidayat <mr.arief.hidayat@gmail.com>
2022-07-09 14:14:27 +0700

    chore: update image URL

--- scripts/db-data.sql
+++ scripts/db-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO videos (title, image_url, video_url, short_desc) VALUES
-  ('Seagulls', 'seagulls.jpeg', 'seagulls.mp4', 'The seagull is considered to be among the most intelligent birds in the world'),
-  ('Yellow Flower', 'yellow-flower.jpeg', 'yellow-flower.mp4', 'Beautiful yellow flower. I do not know the name'),
-  ('A Cup of Coffee', 'cup-of-coffee.jpeg', 'cup-of-coffee.mp4', 'Stirring a cup of coffee with wooden stick'),
-  ('Boat under the bridge', 'boat-under-bridge.jpeg', 'boat-under-bridge.mp4', 'There is a boat under the bridge');
+  ('Seagulls', 'seagulls.jpeg', 'https://d3rhge1c067084.cloudfront.net/01-seagulls/01-seagulls.m3u8', 'The seagull is considered to be among the most intelligent birds in the world'),
+  ('Yellow Flower', 'yellow-flower.jpeg', 'https://d3rhge1c067084.cloudfront.net/02-yellow-flower/02-yellow-flower.m3u8', 'Beautiful yellow flower. I do not know the name'),
+  ('A Cup of Coffee', 'cup-of-coffee.jpeg', 'https://d3rhge1c067084.cloudfront.net/03-cup-of-coffee/03-cup-of-coffee.m3u8', 'Stirring a cup of coffee with wooden stick'),
+  ('Boat under the bridge', 'boat-under-bridge.jpeg', 'https://d3rhge1c067084.cloudfront.net/04-boat-under-bridge/04-boat-under-bridge.m3u8', 'There is a boat under the bridge');
 
